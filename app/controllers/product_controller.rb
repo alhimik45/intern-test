5557eb0f5d1ae83a00dc51ad0fd7650ac52f3745
alhimik45 <alhimik45@gmail.com>
2015-09-19 02:46:58 +0600

    adding products

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -3,7 +3,20 @@ class ProductController < ApplicationController
     @products = Product.all
   end
 
+  def new
+    @product = Product.new
+  end
+
   def create
+    p = post_params
+    unless p['photo'].nil?
+      path = upload_path p['photo']
+      File.open('public' + path, "wb") { |f| f.write(p['photo'].tempfile.read) }
+      p['photo'] = path
+    end
+    @product = Product.new p
+    @product.save!
+    redirect_to :back
   end
 
   def show
@@ -11,4 +24,17 @@ class ProductController < ApplicationController
 
   def delete
   end
+
+  private
+  def post_params
+    params.require(:product).permit(:name, :description, :photo)
+  end
+
+  def upload_path file
+    dir = '/upload/'
+    file_extension = file.original_filename.split('.')[-1]
+    filename = (0...32).map { (65 + rand(26)).chr }.join
+    dir + filename + '.' + file_extension
+  end
+
 end
