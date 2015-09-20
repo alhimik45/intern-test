af0037d7cef9b02c23b3c130ab7e0af746bef4ef
alhimik45 <alhimik45@gmail.com>
2015-09-20 14:18:08 +0600

    make shop registration form

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,3 +1,8 @@
 class Shop < User
+  
+  mount_uploader :avatar, AvatarUploader
+
   validates_length_of :password, {minimum: 8}
+  validates_presence_of :shopname
+  validates_presence_of :avatar
 end
