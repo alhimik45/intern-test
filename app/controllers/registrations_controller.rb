2f1a4e1c9695b4a401bec82ac0e422a4eb985c16
alhimik45 <alhimik45@gmail.com>
2015-09-20 12:59:06 +0600

    add some fields to admin

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,7 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def sign_up(resource_name, resource)
+    #sign_in(resource_name, resource)
     sign_in(:user, resource)
   end
 end
--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,3 +1,8 @@
 class Admin < User
   validates_length_of :password, {minimum: 10}
+  validates_presence_of :firstname
+  validates_presence_of :lastname
+  validates_presence_of :birthday
+  validates_presence_of :avatar
+  validates_presence_of :passport
 end
